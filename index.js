76fc63574e57d67aa89369fdf4748fcc3bf81be3
MarcosRS <MarcosRS16@gmail.com>
2019-08-04 20:16:57 -0400

    check: args

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const minimist = require('minimist')
 const error = require('./utils/error')
 
 module.exports = () => {
+ 
   const args = minimist(process.argv.slice(2))
-
   let cmd = args._[0] || 'help'
+  
+  console.log('process.argv', process.argv)
+  console.log('args._ ', args._)
 
   if (args.version || args.v) {
     cmd = 'version'
